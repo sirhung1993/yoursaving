cdf57df5a8f93c92902ea2f739129b9a515f3d6a
hungadmin <sirhung1993@gmail.com>
2017-02-17 23:05:01 +0700

    try to config HTTPs but fail

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 const express       = require ('express');
 const app           = require ('express')();
 const form          = require('multer')();
+const fs            = require('fs');
+const http          = require('http');
+const https         = require('https');
 const bodyParser    = require ('body-parser');
-const UserIncome    = require ('./abstract/user_income.js')
+const UserIncome    = require ('./abstract/user_income.js');
 
 app.set('port', (process.env.PORT || 5000));
 app.use (bodyParser.urlencoded({ extended : false }));
@@ -46,6 +49,29 @@ app.get('*', (req , res, next) => {
     res.render('pages/error_page');
 })
 
-app.listen(app.get('port') , () => {
-    console.log ('Server is running on port : ', app.get('port'));
-});
+const sslAuthentication = {
+    key: fs.readFileSync('./certificate/server.key' ,'utf8'),
+    cert: fs.readFileSync('certificate/server.crt' , 'utf8')
+};
+
+// const httpServer    = http.createServer(app);
+// const httpsServer   = https.createServer(sslAuthentication, app);
+
+// http.createServer(app).listen(3000, function() {
+//     console.log('Express HTTP server listening on port ' + 3000);
+// });
+//
+// https.createServer(sslAuthentication, app).listen(4000, function() {
+//     console.log('Express HTTPS server listening on port ' + 4000);
+// });
+
+https.createServer(sslAuthentication, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8000);
+
+// app.listen(app.get('port') , () => {
+//     console.log ('Server is running on port : ', app.get('port'));
+//     console.log(sslAuthentication.key.toString());
+//     console.log(sslAuthentication.cert);
+// });
